0c58a7f5116ce1b2ecee46b4b1230981aa9302d0
SakshyamB <sakshyamb23@gmail.com>
2026-01-19 21:05:50 +0545

    Fixed the props passing bug

--- src/Componets/Nav.jsx
+++ src/Componets/Nav.jsx
@@ -3,8 +3,7 @@ import { MdFastfood } from "react-icons/md";
 import { IoSearch } from "react-icons/io5";
 import { LuShoppingBag } from "react-icons/lu";
 
-const Nav = ({ Search, setSearch, setcart, cartItems }) => {
-  const cartCount = cartItems.length;
+const Nav = ({ Search, setSearch, setcart}) => {
   return (
     <div className="w-full h-15 md:h-25 bg-red-200 flex justify-between items-center px-2 md:px-8">
       <div className="w-10 md:h-15 md:w-15 rounded-md shadow-xl h-10 bg-white flex justify-center items-center">
@@ -21,11 +20,10 @@ const Nav = ({ Search, setSearch, setcart, cartItems }) => {
         />
       </form>
       <div className="w-10 md:w-15 relative h-10 md:h-15 rounded-md shadow-xl bg-white flex justify-center items-center">
-        {cartCount > 0 && (
           <span className="absolute top-0 right-1 md:top-2 text-white bg-red-500 font-bold md:text-[18px] text-[12px] w-5 h-5 flex items-center justify-center rounded-full">
-            {cartCount}
+
           </span>
-        )}
+        )
         <LuShoppingBag
           onClick={() => setcart(true)}
           className="md:w-7.5 md:h-7.5 top-3 h-4 w-4 text-green-500"
